b386cde4806c4f56674a3bad0fa3102bd7b894a1
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-02 16:42:17 +0700

    reducer:edit,delete

--- src/contexts/TodoContext.jsx
+++ src/contexts/TodoContext.jsx
@@ -3,7 +3,7 @@ import * as TodoAPIServices from '../services/todoServices';
 import { getSevenDayRange } from '../utils/DateUtils';
 import todoReducer from '../reducers/todoReducer';
 import { INIT_TODO } from '../reducers/todoReducer';
-import { FETCH_TODO, ADD_TODO } from '../reducers/todoReducer';
+import { FETCH_TODO, ADD_TODO, EDIT_TODO, DELETE_TODO } from '../reducers/todoReducer';
 // Create Context => Context Object (NAME)  ใช้ได้ 2 ที่
 // #1 Provider : Wrapper Component => Shared Data,Logic ได้
 // #2 Consumer : Component ที่ต้องการใช้ Data,Logic (Subscribe Component)
@@ -54,51 +54,20 @@ function TodoContextProvider(props) {
 
     // PUT : edit
     const editTodo = async (todoId, updateObj) => {
-        // #1 Sync With External State/Service : Database
-        // #2 Sync with Internal State : UI State
-        // #3 Error Handler eg. modal Error, Sweat Alert
-
         try {
-            // #1 Sync With External State/Service : Database
-            // const response = await axios.put(`http://localhost:8080/todos/${todoId}`, updateObj);
             const response = await TodoAPIServices.updateTodo(updateObj);
-            const updatedTodoObj = response.data.todo;
-
-            // #2  Sync with Internal State : UI State
-            const foundedIndex = todos.findIndex((todo) => todo.id === todoId);
-            if (foundedIndex !== -1) {
-                const newTodoLists = [...todos];
-                // newTodoLists[foundedIndex] = { ...newTodoLists[foundedIndex], ...updatedTodoObj };
-                newTodoLists[foundedIndex] = Object.assign(
-                    {},
-                    newTodoLists[foundedIndex],
-                    updatedTodoObj
-                );
-                setTodos(newTodoLists);
-                setTodosFilter(newTodoLists);
-            }
+            dispatch({ type: EDIT_TODO, payload: { id: todoId, updatedTodo: response.data.todo } });
         } catch (error) {
-            // #3 Error Handler eg. modal Error, Sweat Alert
             console.log(error.response.data);
         }
     };
 
     // DELETE : delete
     const deleteTodo = async (todoId) => {
-        // #1 Sync With External State/Service : Database
-        // #2 Sync with Internal State : UI State
-        // #3 Error Handler eg. modal Error, Sweat Alert
         try {
-            // #1 Sync With External State/Service : Database
-            // await axios.delete(`http://localhost:8080/todos/${todoId}`)
             await TodoAPIServices.deleteTodo(todoId);
-
-            // #2 Sync with Internal State : UI State
-            const newTodoLists = todos.filter((todo) => todo.id !== todoId);
-            setTodos(newTodoLists);
-            setTodosFilter(newTodoLists);
+            dispatch({ type: DELETE_TODO, payload: { id: todoId } });
         } catch (error) {
-            // #3 Error Handler eg. modal Error, Sweat Alert
             console.log(error.response.data);
         }
     };
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,6 +1,8 @@
 // action.type : ประเภทความผิด
 export const FETCH_TODO = 'FETCH_TODO';
 export const ADD_TODO = 'ADD_TODO';
+export const EDIT_TODO = 'EDIT_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
 
 // init state : คะแนนตั้งต้น
 export const INIT_TODO = {
@@ -23,7 +25,27 @@ function todoReducer(state, action) {
                 todos: newTodoList,
                 todosFilter: newTodoList,
             };
-
+        case EDIT_TODO:
+            const { id, updatedTodo } = action.payload;
+            const foundedIndex = state.todos.findIndex((todo) => todo.id === id);
+            if (foundedIndex === -1) return state;
+            const updatedTodoList = [...state.todos];
+            updatedTodoList[foundedIndex] = Object.assign(
+                {},
+                updatedTodoList[foundedIndex],
+                updatedTodo
+            );
+            return {
+                todos: updatedTodoList,
+                todosFilter: updatedTodoList,
+            };
+        case DELETE_TODO:
+            const { id: deleltedId } = action.payload;
+            const restTodoList = [...state.todos].filter((todo) => todo.id !== deleltedId);
+            return {
+                todos: restTodoList,
+                todosFilter: restTodoList,
+            };
         default:
             return state;
     }
