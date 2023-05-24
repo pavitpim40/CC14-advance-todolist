117b5b3c65b6d96080688eaa741ce98c553a8984
Pavit Pimchanagul <pavitpim@gmail.com>
2023-05-24 15:43:31 +0700

    fetch todo

--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -1,13 +1,20 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './TodoLists.module.scss';
-
 import { TodoItem } from './TodoItem';
+import { fetchTodo } from '../../stores/slices/todoSlice';
 
 export function TodoLists() {
+    const dispatch = useDispatch();
+    const { todosFilter } = useSelector((state) => state.todo);
 
+    useEffect(() => {
+        dispatch(fetchTodo());
+    }, []);
 
     return (
         <ul className={styles.todoList}>
-            {[].map((item) => (
+            {todosFilter.map((item) => (
                 <TodoItem todo={item} key={item.id} />
             ))}
         </ul>
--- src/stores/slices/todoSlice.js
+++ src/stores/slices/todoSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import * as TodoServices from '../../services/todoServices';
 
 const initialTodo = {
     todos: [],
@@ -9,7 +10,12 @@ export const todoSlice = createSlice({
     name: 'todo',
     initialState: initialTodo,
     reducers: {
-        getTodo: () => {},
+        getTodo: (state, action) => {
+            const { todos } = action.payload;
+            console.log(todos);
+            state.todos = todos;
+            state.todosFilter = todos;
+        },
         addTodo: () => {},
         editTodo: () => {},
         deleteTodo: () => {},
@@ -20,3 +26,20 @@ export const todoSlice = createSlice({
 
 // ติดตั้งที่ configure Store
 export default todoSlice.reducer;
+
+// ACTION CREATOR : Function ที่สร้าง Action Object = {type:"todo/blabla", payload:arg }
+const { getTodo } = todoSlice.actions;
+
+// ################################################
+// Async Task
+export function fetchTodo() {
+    return async (dispatch) => {
+        try {
+            const response = await TodoServices.getAllTodos();
+            // dispatch : for update store
+            dispatch(getTodo({ todos: response.data.todos }));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+}
